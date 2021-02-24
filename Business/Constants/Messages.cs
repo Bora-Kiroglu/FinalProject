8b61ded3821017e8806a712ba0c10d48e850ea01
Bora Kıroğlu <kartopu-2005@hotmail.com>
2021-02-25 00:08:08 +0300

    24 Şubat ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,9 +8,11 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
-
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Aynı isimde bir ürünü ekleyemezsiniz";
+        public static string CategoryLimitExceed = "15'den fazla kategori olduğu için ürün ekleyemezsiniz";
     }
 }
